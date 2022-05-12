9e2f3923d2bb887f1bb66a843db94bd2b16f7cc9
Jesse Alltop <alltopafi@gmail.com>
2022-05-12 15:15:51 -0500

    update keymap right side

--- keyboards/alltopafi/keymaps/default/keymap.c
+++ keyboards/alltopafi/keymaps/default/keymap.c
@@ -14,15 +14,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT( //TODO how do we allow the left side to work without the right side???
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_ESC  ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,                                             KC_2    ,KC_2    ,KC_2    ,KC_2    ,KC_2    ,KC_2 ,
+     KC_ESC  ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,                                             KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_2 ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,KC_LBRC ,                           KC_2    ,KC_2    ,KC_2    ,KC_2    ,KC_2    ,KC_2    ,KC_2  ,
+     KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,KC_LBRC ,                           KC_RBRC,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_2  ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_CAPS ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,KC_MAC_PASTE  ,                          KC_2 ,KC_2    ,KC_2    ,KC_2    ,KC_2    ,KC_2 ,KC_2 ,
+     KC_CAPS ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,KC_MAC_COPY  ,                KC_MAC_PASTE ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_2    ,KC_2 ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT    ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_DEL ,KC_MUTE ,        KC_2 ,KC_2    ,KC_2    ,KC_2    ,KC_2 ,KC_2  ,KC_2 ,KC_2 ,
+     KC_LSFT    ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_DEL ,KC_MUTE ,        KC_2    ,KC_2    ,KC_N    ,KC_M    ,KC_2    ,KC_2    ,KC_2    ,KC_2 ,
   //├────────┼────────┼──────56──┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
-     TG(1) ,KC_MAC_COPY  ,KC_LCTL ,KC_LALT      ,KC_LGUI     ,KC_SPC ,KC_BSPC  ,        KC_2  ,KC_2  ,    KC_2 ,     KC_2 ,KC_2 ,KC_2   ,KC_2
+     TG(1) ,KC_MAC_COPY  ,KC_LCTL ,KC_LALT      ,KC_LGUI     ,KC_SPC ,KC_BSPC  ,     KC_2    ,KC_2        ,KC_2         ,KC_2    ,KC_2    ,KC_2    ,KC_2
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   ),
   [_SECONDARY] = LAYOUT( //TODO how do we allow the left side to work without the right side???
